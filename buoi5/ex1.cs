ae8bd5cec04838f7ff89d23f159612c8502d6921
TuongVy0511 <vyluong.31251026827@st.ueh.edu.vn>
2026-05-21 22:05:29 +0700

    hoàn thành buổi 5

--- buoi5/ex1.cs
+++ buoi5/ex1.cs
@@ -1,10 +1,13 @@
-﻿using System.Transactions;
+﻿using System.ComponentModel.Design;
+using System.Text;
+using System.Transactions;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        int[] array = { 10, 12, 5, 4, 6, 7, 8, 11, 50, 66, 34, 2, 34, };
+        Console.OutputEncoding = Encoding.UTF8;
+        int[] array = { 10, 12, 5, 4, 6, 7, 8, 11, 50, 66, 34, 2, 34, 7,8,4,};
         //bài1
         double tb = average(array);
         Console.WriteLine($"Giá trị trung trình của array là: {tb}");
@@ -21,13 +24,54 @@ internal class Program
         int x = int.Parse(Console.ReadLine());
         int check1 = find(array, x);
         Console.WriteLine($"Vị trí của {x} trong mảng là {check1}");
-        //bài5
-        int[] lnnn = findmaxmin(array);
+        //bài4
+        Console.WriteLine("Nhập vào 1 số xần xóa khỏi mảng");
+        int z=int.Parse(Console.ReadLine());
+        int[] finalarray = delete(array, z);
+        if (kiemtratontai(array, z))
+
+        {
+            foreach (int z1 in finalarray)
+            {
+                Console.WriteLine(z1 + " ");
+            }
+        }
+        else { Console.WriteLine($"Số {z} không tồn tại trong mảng"); }
+        
+            //bài5
+            int[] lnnn = findmaxmin(array);
         Console.WriteLine($"Giá trị lớn nhất trong mảng là {lnnn[0]}");
         Console.WriteLine($"Giá trị nhỏ nhất trong mảng là {lnnn[1]}");
         //bài6
-
-
+        Console.WriteLine($"Mảng sau khi đảo ngược là ");
+        array = daonguoc(array);
+        Console.WriteLine(string.Join(" ",array));
+        //bài7
+        int[] cacsotrung = timphantutrunglap(array);
+        if(cacsotrung.Length==0)
+        {
+            Console.WriteLine("Mảng không có phần tử nào giống nhau");
+        }    
+        else
+        {
+            Console.WriteLine("các phần tử trùng lặp với nhau là");
+            Console.WriteLine(string.Join(" ",cacsotrung));
+        }    
+        //bài7 dùng list
+        List<int> cacsotrung1= timphantutrunglap1(array);
+        if(cacsotrung1.Count==0)
+        {
+            Console.WriteLine("Mảng không có phần tử nào trùng hết");
+        }
+        else 
+        {
+            Console.WriteLine("Các phần tử trùng nhau là");
+            Console.WriteLine(string.Join(" ",cacsotrung1));
+        }
+        //bài8
+        List<int> mangduynhat = xoaphantutrunglap(array);
+        Console.WriteLine("Mảng sau khi đã xóa các phần tử trùng lặp là");
+        Console.WriteLine(string.Join(" ",mangduynhat));
 
     }
     //logic1
@@ -62,6 +106,28 @@ internal class Program
         return -1;
     }
 
+    //logic4
+    static int[] delete(int[] array,int n)
+    {
+        int conlai = 0;
+        foreach(int x in array)
+        {
+            if (x != n)
+                conlai++;
+        }
+        int[] mangmoi = new int[conlai];
+        int j = 0;
+        for(int i=0;i<array.Length;i++)
+        {
+            if(array[i] == n)
+            { continue; }
+            mangmoi[j]=array[i];
+            j++;
+        }    
+        return mangmoi;
+    }
+
+
     //logic5
     static int[] findmaxmin(int[] array)
     {
@@ -79,5 +145,72 @@ internal class Program
     }
 
     //logic6
+    static int[] daonguoc(int[] array)
+    {
+        int[] mangnguoc=new int[array.Length];
+        int j = 0;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            mangnguoc[j] = array[i];
+            j++;
+        }
+        return mangnguoc;
+    }
+    //logic7
+    static int[] timphantutrunglap(int[] array)
+    {
+        int[] mangchua=new int[array.Length];
+        int count = 0;
+        for(int i=0; i<array.Length; i++)
+        {
+            for(int j=i+1; j<array.Length; j++)
+            {
+                if (array[i]==array[j])
+                {
+                    if(!mangchua.Contains(array[i]))
+                    {
+                        mangchua[count] = array[i];
+                        count++;
+                    }
+                }   
+            }  
+        }
+        int[] ketqua = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            ketqua[i] = mangchua[i];
+        }
+        return ketqua;
+    }
+    //logic7-dùng list
+    static List<int> timphantutrunglap1(int[] array)
+    {
+        List<int> mangchua1 = new List<int>();
+        for(int i=0;i<array.Length;i++)
+        {
+            for(int j=i+1; j<array.Length;j++)
+            {
+                if (array[i] == array[j] && !mangchua1.Contains(array[i]))
+                {
+                    mangchua1.Add(array[i]);
+                    break;
+                }    
+            }    
+        }
+        return mangchua1;
+    }
 
+    //logic8
+    static List<int> xoaphantutrunglap(int[] array)
+    {
+        List<int> ketquasauxoa=new List<int>();
+        foreach(int i in array)
+        {
+            if(!ketquasauxoa.Contains(i))
+            {
+                ketquasauxoa.Add(i);
+            }    
+        }
+        return ketquasauxoa;
+    }
 }
---
+++ buoi5/ex2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace buoi5
+{
+    internal class ex2
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] array = { 10, 12, 5, 4, 6, 7, 8, 11, 50, 66, 34, 2, 34, 7, 8, 4, };
+            //bài1
+            List<int> dasapxep = noibottangdan(array);
+            Console.WriteLine("mảng mới sau khi đã sắp xếp là");
+            Console.WriteLine(string.Join(" ",dasapxep));
+
+            //bài2
+            Console.WriteLine("Nhập vào 1 số bạn muốn tìm trong mảng");
+            int x= int.Parse(Console.ReadLine());
+
+            int vitri = timkiem(array, x);
+            if(vitri==-1)
+            {
+                Console.WriteLine($"số cần tìm không tồn tại trong mảng");
+            }
+            else 
+            {
+                Console.WriteLine($"số cần tìm ở vị trí {vitri}");
+            }
+
+        }
+
+        //logic1
+        static List<int> noibottangdan(int[] array)
+        {
+            List<int> saukhixep=new List<int>(array);
+            for(int i=0;i<saukhixep.Count-1;i++)
+            {
+                for(int j=0;j<saukhixep.Count-1;j++)
+                {
+                    if (saukhixep[j] > saukhixep[j+1])
+                    {
+                        int temp = saukhixep[j];
+                        saukhixep[j]=saukhixep[j+1];
+                        saukhixep[j + 1] = temp;
+                    }    
+                }    
+            }
+            return saukhixep;
+        }
+
+        //logic2
+        static int timkiem(int[] array, int n)
+        {
+            for(int i=0;i <array.Length;i++)
+            {
+                if (array[i]==n)
+                {
+                    return i;
+                }    
+            } 
+            return -1; 
+        }
+
+    }
+}
