abe1fd4c5a8eebe5764c230bb9bf38f549748e4d
TuongVy0511 <vyluong.31251026827@st.ueh.edu.vn>
2026-05-28 00:28:28 +0700

    hàm răng cưa

---
+++ buoi6/extra-mangrangcua.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buoi6
+{
+    internal class extra_mangrangcua
+    {
+        //nắm cách khởi tạo mảng răng cưa thôi không cần làm các bài tập khó vì răng cưa không co trong đề thi
+       public static void Main(string[] arg)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập vào số hàng bạn muốn tạo mảng");
+            int dong = int.Parse(Console.ReadLine());
+            int[][] a = Mangrandom(dong);
+            Console.WriteLine("Mảng sau khi nhập là ");
+            inmangrangcua(a);
+        }
+        //logic in mảng
+        static void inmangrangcua(int[][]a)
+        {
+            for(int i=0;i<a.Length;i++)
+            {
+                for(int j = 0; j < a[i].Length;j++)
+                {
+                    Console.Write($"{a[i][j]}\t");
+                }
+                Console.WriteLine();
+            }    
+        }
+
+        //logic random mảng
+        static int[][] Mangrandom(int dong)
+        {
+            int[][] a= new int[dong][];
+            Random ngaunhien = new Random();
+            for(int i=0;i<dong;i++)
+            {
+                Console.WriteLine($"Nhập và số phần tử của hàng {i}");
+                int cotcuahang = int.Parse(Console.ReadLine());
+                a[i] = new int[cotcuahang];
+                for(int j=0;j<cotcuahang;j++)
+                { a[i][j] = ngaunhien.Next(0, 100); }    
+            }
+            return a;
+        }
+    }
+}
