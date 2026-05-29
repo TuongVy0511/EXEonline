b87df442a250a9951176b8888674852f409df588
TuongVy0511 <vyluong.31251026827@st.ueh.edu.vn>
2026-05-29 14:38:21 +0700

    chuỗi

--- buoi7/ex-slide.cs
+++ buoi7/ex-slide.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.AccessControl;
 using System.Text;
 
 namespace buoi7
@@ -11,9 +12,12 @@ namespace buoi7
 
         {
             //1.Nhập và in chuỗi ra màn hình
-            Console.OutputEncoding=Encoding.UTF8;
-            string chuoi = " Hello word. My name is Vy. I love everyone. Thank you so much!";
-            Console.WriteLine($"{chuoi}");
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập vào 1 chuỗi");
+            string chuoi = Console.ReadLine();
+
+
 
             //2.Tìm độ dài của chuỗi bằng hàm/không hàm
             Console.WriteLine();
@@ -30,24 +34,66 @@ namespace buoi7
             indaonguoc(chuoi);
 
             //5.Tổng số từ có trong 1 chuỗi
-
-
+            int tongsotu = countwords(chuoi);
+            Console.WriteLine($"Số từ có trong câu là : {tongsotu}");
             //6.so sánh 2 chuỗi với nhau mà không sử dụng hàm
-           
+            Console.WriteLine("Nhập vào chuỗi mới để so sánh ");
+            string chuoi1 = Console.ReadLine();
 
+            int kqasosanh = sosanh(chuoi, chuoi1);
+            Console.WriteLine("Kết quả so sánh");
+            if (kqasosanh == 0)
+            {
+                Console.WriteLine("2 chuỗi giống nhau y hệt");
+            }
+
+            else if (kqasosanh < 0)
+            {
+                Console.WriteLine("Chuỗi đầu nhỏ hơn chuỗi sau");
+                Console.WriteLine($"Trong từ điển, {chuoi} sẽ đứng trước {chuoi1}");
+            }
+            else
+            {
+                Console.WriteLine("Chuỗi đầu lớn hơn chuỗi sau");
+                Console.WriteLine($"Trong từ điển , {chuoi} sẽ đứng sau {chuoi1}");
+            }
             //7.đếm số lượng chữ cái , chữ số và kí tự đặc biệt
+            //char.IsLetter, char.IsDigit
+            Console.WriteLine("Phân loại kí tự");
+            phanloai(chuoi);
+
             //8.đếm số nguyên âm , phụ âm trong chuỗi
-            //9.Kiểm tra 1 chuỗi con xem có tồn tại trong chuỗi ban đầu không
-            //10.Tìm kiếm vị trí xuất hiện của 1 chuỗi con trong chuỗi ban đầu
+            Console.WriteLine();
+            demnguynphu(chuoi);
+
+            //9 10 và 12.Kiểm tra 1 chuỗi con xem có tồn tại trong chuỗi ban đầu không, nêu vị trí và xem xuất hiện bao nhiêu lần
+            //chuoi.Contains(chuoicon);
+            Console.WriteLine("Nhập vào 1 từ cần tìm trong chuỗi");
+            string chuoi2 = Console.ReadLine();
+            Console.WriteLine("Dò tìm");
+            chuoicon(chuoi, chuoi2);
+
             //11.Kiểm tra xem 1 kí tự có phải là chữ cái hay không , nếu là chữ cái kiểm tra là chữ hoa hay thường
-            //12.Tìm số lần xuất hiện của chuỗi con trong chuỗi ban đầu
+            Console.WriteLine("Nhập vào 1 ký tự để kí tự để kiểm tra");
+            char nhap = (char)Console.Read();
+            kiemtrathuongin(nhap);
+            Console.ReadLine();
             //13.Chèn 1 chuỗi con vào trước vị trí xuất hiện đầu tiên của 1 chuỗi con khác
+            Console.WriteLine();
+            Console.WriteLine("Nhập vào chuỗi cần chèn");
+            string chuoichen = Console.ReadLine();
+            Console.WriteLine("Chèn chuỗi này vào trước từ nào");
+            string vitrichen = Console.ReadLine();
+
+            Console.WriteLine("Chuỗi sau khi được chèn là");
+            string chuoiafter = chenchuoi(chuoi, chuoichen, vitrichen);
+            Console.WriteLine($"{chuoiafter}");
         }
         //logic 2
         static int dodai(string chuoi)
         {
             int lenght = 0;
-            foreach(char c in chuoi)
+            foreach (char c in chuoi)
             {
                 lenght++;
             }
@@ -57,21 +103,172 @@ namespace buoi7
         //logic3
         static void in_mỗi_chữ(string chuoi)
         {
-            for(int i=0;i<chuoi.Length;i++)
+            for (int i = 0; i < chuoi.Length; i++)
             {
-                Console.Write(chuoi[i]+" ");
-            }    
+                Console.Write(chuoi[i] + " ");
+            }
         }
-        
+
         //logic4
         static void indaonguoc(string chuoi)
         {
-            for(int i=chuoi.Length-1;i>=0;i--)
+            for (int i = chuoi.Length - 1; i >= 0; i--)
             {
                 Console.Write($"{chuoi[i]}");
-            }    
+            }
         }
-        //logic5
-        
+        //logic5 đếm số từ có trong câu
+        static int countwords(string s)
+        {
+            int sotu = 0;
+            bool trongtu = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == '.' || c == '!' || c == ';')
+                {
+                    trongtu = false;
+                }
+                else
+                {
+                    if (trongtu == false)
+                    {
+                        sotu++;
+                        trongtu = true;
+                    }
+                }
+
+            }
+            return sotu;
+        }
+        //logic6 so sánh 2 chuỗi
+        static int sosanh(string s1, string s2)
+        {
+            int minlength = s1.Length < s2.Length ? s1.Length : s2.Length;
+            for (int i = 0; i < minlength; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    return s1[i] - s2[i];
+                }
+            }
+            return s1.Length - s2.Length;
+        }
+
+        //loic 7 đếm số chữ cái , chữ số và kí tự đặc biệt
+        static void phanloai(string s)
+        {
+            int chu = 0;
+            int so = 0;
+            int dacbiet = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    chu++;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    so++;
+                }
+                else if (c != ' ' && c != '\t' && c != '\n')
+                {
+                    dacbiet++;
+                }
+            }
+            Console.WriteLine($"Số chữ cái là : {chu}");
+            Console.WriteLine($"Số chữ số là : {so}");
+            Console.WriteLine($"Số ký tự đặc biệt là : {dacbiet}");
+        }
+
+        //logic 8 đếm số nguyên âm và phụ âm
+        static void demnguynphu(string s)
+        {
+            int nguyen = 0;
+            int phu = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char C = s[i];
+                char c = char.ToLower(C);
+                if (c >= 'a' && c <= 'z')
+                {
+                    if (c == 'u' || c == 'e' || c == 'o' || c == 'a' || c == 'i')
+                    {
+                        nguyen++;
+                    }
+                    else
+                    {
+                        phu++;
+                    }
+                }
+
+            }
+            Console.WriteLine($"Số nguyên âm là :{nguyen}");
+            Console.WriteLine($"Số phụ âm là : {phu}");
+        }
+
+        //logic 9 10 12 kiểm tra chuỗi con có tồn tại trong chuỗi ban đầu hay không
+        static void chuoicon(string s, string s1)
+        {
+            if (s.Length < s1.Length || string.IsNullOrEmpty(s1))
+            {
+                Console.WriteLine($"\"{s1}\" không xuất hiện trong chuỗi chính");
+                return;
+            }
+            int solanxuathien = 0;
+            int vitrixuathien = 0;
+            while ((vitrixuathien = s.IndexOf(s1, vitrixuathien)) != -1)
+            {
+                solanxuathien++;
+                Console.WriteLine($"tìm thấy tại vị trí {vitrixuathien}");
+
+                vitrixuathien += s1.Length;
+            }
+            if (solanxuathien > 0)
+            {
+                Console.WriteLine($"Tổng cộng \"{s1}\" xuất hiện {solanxuathien} lần");
+            }
+            else
+            {
+                Console.WriteLine($" \"{s1}\" không xuất hiện trong chuỗi gốc");
+            }
+        }
+
+            //logic11
+            static void kiemtrathuongin(char c)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    Console.WriteLine($" Ký tự '{c}' là một chữ cái.");
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        Console.WriteLine("  Và đây là CHỮ THƯỜNG");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Và đây là CHỮ HOA ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"- Ký tự '{c}' KHÔNG PHẢI là chữ cái!");
+                }
+            }
+            //logic bài 13
+            static string chenchuoi(string s, string s1, string vitrichen)
+            {
+                int vitri = s.IndexOf(vitrichen);
+                if (vitri == -1)
+                {
+                    Console.WriteLine($"Không tìm thấy \"{vitrichen}\" để chèn");
+                    return s;
+                }
+                string dau = s.Substring(0, vitri);
+                string cuoi = s.Substring(vitri-1);
+                return dau + s1 + cuoi;
+            }
+
+
+        }
     }
-}
---
+++ buoi7/ex-string.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buoi7
+{
+    internal class ex_string
+    {
+        public static void Main(string[] arg)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập vào 1 chuỗi gốc để thực hiện");
+            string s = Console.ReadLine();
+            //Câu 10: Tìm ký tự xuất hiện với tần suất nhiều lần nhất (xuất hiện nhiều nhất) trong một chuỗi.
+            Console.WriteLine("Xét trong chuỗi");
+            tansuat(s);
+            //Câu 11: Sắp xếp một mảng các chuỗi theo thứ tự bảng chữ cái tăng dần (Ascending order).
+            Console.WriteLine("Nhập số lượng chuỗi trong mảng");
+            int n = int.Parse(Console.ReadLine());
+            string[] array = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Nhập chuỗi thứ {i}");
+                array[i] = Console.ReadLine();
+            }
+            // sxarray(array);
+            sxarray1(array);
+            Console.WriteLine("Mảng đã sắp xếp các chuỗi là");
+            Console.WriteLine(string.Join(" ", array));
+
+            //Câu 12: Nhập vào một danh sách các chuỗi từ bàn phím và sắp xếp chúng bằng thuật toán sắp xếp nổi bọt (Bubble Sort).
+            bubarray(array);
+            Console.WriteLine("Mảng đã sắp xếp các chuỗi bằng bubble sort là");
+            Console.WriteLine(string.Join(" ", array));
+
+            //Câu 13: Trích xuất (cắt) một chuỗi con từ chuỗi gốc dựa vào vị trí bắt đầu và độ dài cho trước, không dùng hàm thư viện.
+            Console.WriteLine("Nhập vào từ mà bạn muốn cắt từ chuỗi");
+            string cat = Console.ReadLine();
+            string kqacat = catchuoi(s, cat);
+            Console.WriteLine("Chuỗi sau khi mà cắt là ");
+            Console.WriteLine(kqacat);
+            //Câu 15: Đọc một câu và tiến hành đổi chữ thường thành chữ hoa, chữ hoa thành chữ thường.
+            //Câu 16: Viết chương trình mô phỏng đăng nhập để kiểm tra tính chính xác của Username và Password.
+            Console.WriteLine();
+            bool dangnhap = false;
+            while (dangnhap == false)
+            {
+                Console.Write("Nhập username:"); string user = Console.ReadLine();
+                Console.Write("Nhập password:"); string pass = Console.ReadLine();
+                dangnhap = kiemtra(user, pass);
+                if (dangnhap == false)
+                {
+                    Console.WriteLine("fail, nhập lại");
+                }
+            }
+            Console.WriteLine("Bạn đã đăng nhập thành công");
+           
+            //Câu 24: Nhập vào tên của mọi người và tiến hành sắp xếp danh sách đó theo thứ tự bảng chữ cái của Họ (Last name). 
+        }
+        //logic bài 10 , đếm tần xuất max
+        static void tansuat(string s)
+        {
+            int maxtansuat = 0;
+            char maxkytu = ' ';
+            for(int i=0;i<s.Length; i++)
+            {
+                if (s[i] == ' ') continue;
+
+                int demsolan = 0;
+                int vitri = 0;
+                while ((vitri = s.IndexOf(s[i],vitri))!=-1)
+                {
+                    demsolan++;
+                    vitri++;
+                }    
+                if(demsolan>maxtansuat)
+                {
+                    maxtansuat = demsolan;
+                    maxkytu = s[i];
+                }    
+            }
+            Console.WriteLine($"Ký tự xuất hiện nhiều lần nhất là '{maxkytu}' với số lần xuất hiện là '{maxtansuat}'");
+        }
+        //logic11 sắp xếp vị trí của các chuỗi trong mảng dùng tools
+        static void sxarray(string[] array)
+        {
+            Array.Sort(array);
+        }
+
+        //logic11 sắp xếp vị trí của các chuỗi trong mảng không dùng tools 
+        static void sxarray1(string[] array)
+        {
+            for(int i=0; i<array.Length;i++)
+            {
+                for(int j =i+1;j<array.Length;j++)
+                {
+                    if (array[i].CompareTo(array[j])>0)
+                    {
+                        string temp = array[i];
+                        array[i] = array[j];
+                        array[j]=temp;
+                    }    
+                }    
+            }    
+        }
+        //logic 12 sx theo pubble sort
+        static void bubarray(string[] array)
+        {
+            for (int i=0;i<array.Length-1;i++)
+            {
+                for(int j=0; j< array.Length - 1-i;j++)
+                {
+                    if (array[j].CompareTo(array[j+1])>0)
+                    {
+                        string temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }    
+                }    
+            }    
+        }
+
+        //logic 13 cắt ký tự bằng tools
+        static string catchuoi(string s, string s1)
+        {
+            int vitri = s.IndexOf(s1);
+            if(vitri==-1)
+            {
+                return "Không tìm thấy từ này trong chuỗi";
+            }
+            int dodai = s1.Length;
+            return s.Remove(vitri, dodai);
+        }
+
+        //logic 16 mật khẩu và tên
+        static bool kiemtra(string user, string pass)
+        {
+            string usertrue = "tuongvy0511";
+            string passtrue = "01091997Jk*";
+            if(user==usertrue && pass==passtrue)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
